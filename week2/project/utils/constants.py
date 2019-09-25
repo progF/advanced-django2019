0814cada428ab96b81475bbafb7730cee0db7bde
ProgF <alimbayevaf@gmail.com>
2019-09-25 09:37:53 +0600

    Models added

---
+++ week2/project/utils/constants.py
@@ -0,0 +1,11 @@
+NEW = 0
+TO_DO = 1
+IN_PROGRESS = 2
+DONE = 3
+
+BLOCK_TYPES = (
+        (NEW, 'New'),
+        (TO_DO, 'To Do'),
+        (IN_PROGRESS, 'In Progress'),
+        (DONE, 'Done'),
+)
